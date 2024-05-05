aacc63a3df8cc321c62189b5da1ce9b84f9c55f4
niall swift <niallswift14@gmail.com>
2024-05-05 13:48:12 -0300

    new update

--- App.tsx
+++ App.tsx
@@ -1,17 +1,36 @@
-
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes/index';
 
-import { NavigationContainer } from '@react-navigation/native'
-import Routes from './src/routes/index'
-
-import { AuthProvider } from './src/contexts/AuthContext'
+import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        console.log("Preparando a tela de apresentação...");
+        await SplashScreen.preventAutoHideAsync();
+        console.log("Tela de apresentação preparada.");
+        setTimeout(async () => {
+          console.log("Ocultando a tela de apresentação...");
+          await SplashScreen.hideAsync();
+          console.log("Tela de apresentação oculta.");
+        }, 2000);
+      } catch (e) {
+        console.error("Erro ao preparar a tela de apresentação:", e);
+      }
+    }
+
+    prepare();
+  }, []);
+
   return (
     <NavigationContainer independent={true}>
       <AuthProvider>
-          <StatusBar backgroundColor="#fffdf2" barStyle="dark-content" translucent={false} hidden={true} />
-          <Routes/>
+        <StatusBar backgroundColor="#fffdf2" barStyle="dark-content" translucent={false} hidden={true} />
+        <Routes />
       </AuthProvider>
     </NavigationContainer>
   );
